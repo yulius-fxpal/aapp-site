d5ff9bc4f90b3f46e238419bd37fcda68f643bd0
David A. Shamma <1846+ayman@users.noreply.github.com>
2020-08-31 22:44:24 -0700

    Adding WCAG2.0 hooks for screen readers.

--- pages/adopt.jsx
+++ pages/adopt.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import uuid from 'react-uuid';
+import Head from 'next/head';
 import Link from 'next/link';
 import { stateCounties } from '../data/stateCounties';
 
@@ -44,59 +45,83 @@ export default class Adopt extends Component {
     const { currentState, states, counties } = this.state;
     const statesList = states.length > 0 && states.map(Adopt.makeLIs);
     const countyList = counties.length > 0 && counties.map(Adopt.makeLIs);
+    const title = 'Adopt a Polling Place';
     return (
-      <div className='bg-bg2 bg-cover h-screen '>
-        <div className='container mx-auto h-screen relative'>
-          <div className='flex flex-col w-full text-center'>
-            <div className='mt-10'>
-              <div className='md:flex md:items-center mb-6'>
-                <div className='md:w-5/6' />
-                <div className='md:w-1/6 text-right'>
-                  <Link href='/'>
-                    <button type='submit' className='bg-white active:bg-red-400 hover:bg-blue-700 hover:text-white focus:outline-none focus:shadow-outline text-black py-2 px-4 w-1/1 rounded-full mr-4'>
-                      <img src='/img/star.svg' alt='Home button' style={{ float: 'none', width: '300px' }} />
-                      Main
-                    </button>
-                  </Link>
-                </div>
-              </div>
-              <div className='md:flex md:items-center mb-6'>
-                <div className='md:w-1/4' />
-                <select
-                  onChange={this.handleStateChange}
-                  value={currentState}
-                  className='md:w-1/2 bg-white hover:bg-blue-700 hover:text-white focus:outline-none focus:shadow-outline text-black py-2 px-4 w-1/3 rounded-full'
-                >
-                  {statesList}
-                </select>
-              </div>
-              <div className='md:flex md:items-center mb-6'>
-                <div className='md:w-1/4' />
-                <select className='md:w-1/2 bg-white hover:bg-blue-700 hover:text-white focus:outline-none focus:shadow-outline text-black py-2 px-4 w-1/3 rounded-full'>
-                  {countyList}
-                </select>
-              </div>
-              <div className='md:flex md:items-center mb-6'>
-                <div className='md:w-1/4' />
-                <input className='md:w-1/2 appearance-none bg-white block border border-gray-300 focus:outline-none focus:outline-none focus:shadow-outline focus:shadow-outline hover:bg-blue-700 hover:text-white leading-normal px-4 py-2 rounded-full rounded-lg text-black w-1/3 ' type='email' placeholder='you@example.com' />
-              </div>
-              <div className='text-white absolute p-10 text-center'>
-                <div className='md:flex md:items-center mb-6'>
-                  <div className='md:w-1/4' />
-                  <label className='w-1/2 block font-bold' htmlFor='promiseCheck'>
-                    <p className='text-sm text-left'>
-                      <input className='mr-2 leading-tight' type='checkbox' name='promiseCheck' />
-                      By checking this box you&apos;re promising to help real people cast real
-                      votes. You may receive updates about this project with specific ways you
-                      can help your neighbors as they cast their votes.
-                    </p>
-                  </label>
+      <>
+        <Head>
+          <title>{title}</title>
+          <meta property='og:title' content='{title}' key='title' />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        </Head>
+        <main>
+          <div className='bg-bg2 bg-cover h-screen '>
+            <div className='container mx-auto h-screen relative'>
+              <div className='flex flex-col w-full text-center'>
+                <div className='mt-10'>
+                  <div className='md:flex md:items-center mb-6'>
+                    <div className='md:w-5/6' />
+                    <div className='md:w-1/6 text-right'>
+                      <Link href='/'>
+                        <button type='submit' className='bg-white active:bg-red-400 hover:bg-blue-700 hover:text-white focus:outline-none focus:shadow-outline text-black py-2 px-4 w-1/1 rounded-full mr-4'>
+                          <img src='/img/star.svg' alt='Home button' style={{ float: 'none', width: '300px' }} />
+                          Main
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                  <div className='md:flex md:items-center mb-6'>
+                    <div className='md:w-1/4' />
+                    <select
+                      aria-label='Pick a State'
+                      onChange={this.handleStateChange}
+                      value={currentState}
+                      className='md:w-1/2 bg-white hover:bg-blue-700 hover:text-white focus:outline-none focus:shadow-outline text-black py-2 px-4 w-1/3 rounded-full'
+                    >
+                      {statesList}
+                    </select>
+                  </div>
+                  <div className='md:flex md:items-center mb-6'>
+                    <div className='md:w-1/4' />
+                    <select
+                      aria-label='Pick a County in the State'
+                      className='md:w-1/2 bg-white hover:bg-blue-700 hover:text-white focus:outline-none focus:shadow-outline text-black py-2 px-4 w-1/3 rounded-full'
+                    >
+                      {countyList}
+                    </select>
+                  </div>
+                  <div className='md:flex md:items-center mb-6'>
+                    <div className='md:w-1/4' />
+                    <input
+                      aria-label='Enter your email'
+                      className='md:w-1/2 appearance-none bg-white block border border-gray-300 focus:outline-none focus:outline-none focus:shadow-outline focus:shadow-outline hover:bg-blue-700 hover:text-white leading-normal px-4 py-2 rounded-full rounded-lg text-black w-1/3 '
+                      type='email'
+                      placeholder='you@example.com'
+                    />
+                  </div>
+                  <div className='text-white absolute p-10 text-center'>
+                    <div className='md:flex md:items-center mb-6'>
+                      <div className='md:w-1/4' />
+                      <label className='w-1/2 block font-bold' htmlFor='promise'>
+                        <p className='text-sm text-left'>
+                          <input
+                            className='mr-2 leading-tight'
+                            type='checkbox'
+                            name='promiseCheck'
+                            id='promise'
+                          />
+                          By checking this box you&apos;re promising to help real people cast real
+                          votes. You may receive updates about this project with specific ways you
+                          can help your neighbors as they cast their votes.
+                        </p>
+                      </label>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        </main>
+      </>
     );
   }
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,37 +1,48 @@
 import React, { Component } from 'react';
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default class Home extends Component {
   render() {
+    const title = 'Adopt a Polling Place';
     return (
-      <div className='bg-bg1 bg-cover h-screen '>
-        <div className='h-screen bg-gradient-to-b from-white via-grey-500 to-black-500 opacity-60'>
-          <div className='container mx-auto h-screen relative'>
-            <div className='flex flex-col w-full text-center'>
-              <img src='/img/banner.svg' className='' alt='Decorative banner' />
-              <div className='mt-10'>
-                <Link href='/adopt'>
-                  <button type='submit' className='bg-white active:bg-red-400 hover:bg-blue-700 hover:text-white focus:outline-none focus:shadow-outline text-black py-2 px-4 w-1/5 rounded-full mr-4'>
-                    ADOPT A LOCATION
-                  </button>
-                </Link>
-                <Link href='/adopt'>
-                  <button type='submit' className='bg-white hover:bg-blue-700 hover:text-white focus:outline-none focus:shadow-outline text-black py-2 px-4 w-1/5 rounded-full'>
-                    ROLL CALL
-                  </button>
-                </Link>
+      <>
+        <Head>
+          <title>{title}</title>
+          <meta property='og:title' content='{ title }' key='title' />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        </Head>
+        <main>
+          <div className='bg-bg1 bg-cover h-screen '>
+            <div className='h-screen bg-gradient-to-b from-white via-grey-500 to-black-500 opacity-60'>
+              <div className='container mx-auto h-screen relative'>
+                <div className='flex flex-col w-full text-center'>
+                  <img src='/img/banner.svg' className='' alt='Decorative banner' />
+                  <div className='mt-10'>
+                    <Link href='/adopt'>
+                      <button type='submit' className='bg-white active:bg-red-400 hover:bg-blue-700 hover:text-white focus:outline-none focus:shadow-outline text-black py-2 px-4 w-1/5 rounded-full mr-4'>
+                        ADOPT A LOCATION
+                      </button>
+                    </Link>
+                    <Link href='/adopt'>
+                      <button type='submit' className='bg-white hover:bg-blue-700 hover:text-white focus:outline-none focus:shadow-outline text-black py-2 px-4 w-1/5 rounded-full'>
+                        ROLL CALL
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+                <div className='text-white absolute bottom-0 p-10 text-center'>
+                  Voting is our patriotic duty and our right. Many Americans will be asked
+                  to do too much in order to exercise that right. By adopting a polling place,
+                  you&apos;re promising to support your neighbors as they in the next election.
+                  You&apos;re pledging to bring a few boxes of Pizza, lawn chairs or maybe some
+                  games to help them deal with the unfairly long lines in some communities.
+                </div>
               </div>
             </div>
-            <div className='text-white absolute bottom-0 p-10 text-center'>
-              Voting is our patriotic duty and our right. Many Americans will be asked
-              to do too much in order to exercise that right. By adopting a polling place,
-              you&apos;re promising to support your neighbors as they in the next election.
-              You&apos;re pledging to bring a few boxes of Pizza, lawn chairs or maybe some
-              games to help them deal with the unfairly long lines in some communities.
-            </div>
           </div>
-        </div>
-      </div>
+        </main>
+      </>
     );
   }
 }
